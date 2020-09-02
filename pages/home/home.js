20a3e2e541aa4a672766fb70c25a197e45c90097
陈腾飞 <wb-ctf230293@alibaba-inc.com>
2020-09-02 11:04:52 +0800

    01_初步数据绑定

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,17 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'code why',
+    age: 18,
+    movies: [
+      { id: 110, name: 'kobe', age: 30 },
+      { id: 111, name: 'james', age: 25 },
+      { id: 112, name: 'currry', age: 31 },
+      { id: 113, name: 'why', age: 18 },
+    ],
+    counter: 0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtn() {
+    this.setData({ counter: this.data.counter + 1 })
+    console.log('----->', '按钮发生了点击', new Date());
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
